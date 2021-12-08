132fbebf8fba83ea61c7dcba7015f5ba8f1f2f10
Chai <salihsamet28@gmail.com>
2021-12-08 16:12:46 -0500

    Created Hp Bar - Salih

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,23 +1,22 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+	public float speed = 2f;
 
-    public PlayerMovement controller;
-
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	Vector3 move;
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Input.GetAxisRaw("Horizontal"));
-        
+		move.x = Input.GetAxis("Horizontal");
+		move.z = Input.GetAxis("Vertical");
     }
+
+	void FixedUpdate()
+	{
+		transform.Translate(move * Time.fixedDeltaTime * speed);
+	}
 }
